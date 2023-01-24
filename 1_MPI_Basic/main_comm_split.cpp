3fc6ecaf7f3b4e1506482cabed9848c614a12e1d
fabio.vicini <fabio.vicini@polito.it>
2023-01-24 10:09:09 +0100

    add: new slides

---
+++ 1_MPI_Basic/main_comm_split.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include <mpi.h>
+
+int main(int argc, char **argv)
+{
+    int err;
+    MPI_Init(&argc, &argv);
+    
+    int nprocs, my_rank;
+    
+    // Get the rank and size in the original communicator
+    int world_rank, world_size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    unsigned int color = world_rank / 4; // Determine color based on row
+    
+    // Split the communicator based on the color and use the
+    // original rank for ordering
+    MPI_Comm row_comm;
+    err = MPI_Comm_split(MPI_COMM_WORLD, color, world_rank, &row_comm);
+    
+    int row_rank, row_size;
+    MPI_Comm_rank(row_comm, &row_rank);
+    MPI_Comm_size(row_comm, &row_size);
+    
+    std::cout<< "WORLD RANK/SIZE: "<< world_rank<< "/"<< world_size<< "\t";
+    std::cout<< "ROW RANK/SIZE: "<< row_rank<< "/"<< row_size<< std::endl;
+    
+    MPI_Comm_free(&row_comm);
+    
+    MPI_Finalize();
+    
+    return 0;
+}
