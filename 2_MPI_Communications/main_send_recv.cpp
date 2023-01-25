bbed67e29d6e9afb0a1956aa131aa52570e19a15
fabio.vicini <fabio.vicini@polito.it>
2023-01-25 21:29:08 +0100

    ading 2 lesson

---
+++ 2_MPI_Communications/main_send_recv.cpp
@@ -0,0 +1,31 @@
+
+#include <iostream>
+#include <mpi.h>
+
+int main(int argc, char **argv) 
+{
+    MPI_Init(&argc, &argv);
+    
+    int process_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &process_rank);
+    
+    int tag = 10;
+    float a[2] = { 1.2, 3.4 };
+    float b[2] = { 0.0, 0.0 } ;
+    MPI_Status status;
+    
+    if (process_rank == 0)
+        MPI_Send(&a, 2, MPI_FLOAT, 1, tag, MPI_COMM_WORLD);
+    else if (process_rank == 1)
+        MPI_Recv(&b, 2, MPI_FLOAT, 0, tag, MPI_COMM_WORLD, &status);
+
+    std::cout<< "Process "<< process_rank<< " ";
+    std::cout<< "a "<< a[0]<< ", "<< a[1]<< " ";
+    std::cout<< "b "<< b[0]<< ", "<< b[1]<< std::endl; 
+    std::cout<< "Status SOURCE: "<< status.MPI_SOURCE<< " ";
+    std::cout<< "Status TAG "<< status.MPI_TAG<< std::endl;
+    
+    MPI_Finalize();
+
+    return 0;
+}
