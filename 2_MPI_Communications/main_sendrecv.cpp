71279b156b16b35726324b42dba365160300b9f1
fabio.vicini <fabio.vicini@polito.it>
2023-01-26 16:58:24 +0100

    add: new communications

--- 2_MPI_Communications/main_sendrecv.cpp
+++ 2_MPI_Communications/main_sendrecv.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
 {
     MPI_Init(&argc,&argv);
     
-    int rank, procs;
+    int rank, procs, tag = 10;
     MPI_Comm_size(MPI_COMM_WORLD, &procs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     
@@ -14,8 +14,9 @@ int main(int argc, char **argv)
     int left = rank == 0 ? procs - 1 : rank - 1;
     
     int buffer1 = rank, buffer2 = -1;
-    MPI_Sendrecv(&buffer1, 1, MPI_INT, right, 123, &buffer2, 1, MPI_INT, left,
-    123, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Sendrecv(&buffer1, 1, MPI_INT, right, tag, 
+                 &buffer2, 1, MPI_INT, left, tag, 
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     
     std::cout<< "Process "<< buffer1<< " ";
     std::cout<< "recvs from "<< buffer2<< std::endl; 
