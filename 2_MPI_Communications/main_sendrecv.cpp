81aba0bb45c40ae9593d40a64ac220ae8f244dea
fabio.vicini <fabio.vicini@polito.it>
2023-01-26 15:07:10 +0100

    add: sendrecv

---
+++ 2_MPI_Communications/main_sendrecv.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <mpi.h>
+
+int main(int argc, char **argv) 
+{
+    MPI_Init(&argc,&argv);
+    
+    int rank, procs;
+    MPI_Comm_size(MPI_COMM_WORLD, &procs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    
+    int right = (rank + 1) % procs;
+    int left = rank == 0 ? procs - 1 : rank - 1;
+    
+    int buffer1 = rank, buffer2 = -1;
+    MPI_Sendrecv(&buffer1, 1, MPI_INT, right, 123, &buffer2, 1, MPI_INT, left,
+    123, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    
+    std::cout<< "Process "<< rank<< " ";
+    std::cout<< "buffer1 "<< buffer1<< " ";
+    std::cout<< "buffer2 "<< buffer2<< std::endl; 
+    
+    MPI_Finalize();
+    return 0;
+}
