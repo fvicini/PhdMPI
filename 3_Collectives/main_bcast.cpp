f66f7d652d5705ef1a9b747aa51546397b914024
fabio.vicini <fabio.vicini@polito.it>
2026-09-01 14:37:46 +0200

    still

--- 3_Collectives/main_bcast.cpp
+++ 3_Collectives/main_bcast.cpp
@@ -5,10 +5,10 @@
 int main(int argc, char **argv) 
 {
     MPI_Init(&argc, &argv);
-    
+
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    
+
     MPI_Status status;
     double a[2] = { 0.0, 0.0 };
     if ( rank == 0 ) 
@@ -16,13 +16,13 @@ int main(int argc, char **argv)
         a[0] = 2.1; 
         a[1] = 4.3;
     }
-    
+
     // send the information to all the other processes
     MPI_Bcast(a, 2, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    
+
     std::cout<< "Process "<< rank<< " ";
     std::cout<< "a "<< a[0]<< ", "<< a[1]<< std::endl; 
-    
+
     MPI_Finalize();
     return 0;
 }
